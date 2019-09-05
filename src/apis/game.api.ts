add0d428d2d4e93cf840fb13fabb14186ee9e497
Cody Cahoon <codyjcahoon@gmail.com>
2019-09-04 22:40:49 -0400

    feat: moved types into api namespace, added to string methods to api's

--- src/apis/game.api.ts
+++ src/apis/game.api.ts
@@ -1,31 +1,71 @@
-import { Character, Game, GameTheme } from '../types';
 import { IdUtil } from '../utils/id.util';
 import { CharacterAPI } from './character.api';
+import { PlayerAPI } from './player.api';
 
 export namespace GameAPI {
+    export interface Game {
+        id: string;
+
+        murderers: PlayerAPI.Player[];
+        players: PlayerAPI.Player[];
+        theme: GameTheme;
+    }
+
+    export enum GameTheme {
+        Western = 'A Wild West',
+    }
+
+    export const themeAvailableCharacters: Record<
+        GameTheme,
+        CharacterAPI.Character[]
+    > = {
+        [GameTheme.Western]: [
+            CharacterAPI.init('Western Character #1'),
+            CharacterAPI.init('Western Character #2'),
+            CharacterAPI.init('Western Character #3'),
+            CharacterAPI.init('Western Character #4'),
+            CharacterAPI.init('Western Character #5'),
+            CharacterAPI.init('Western Character #6'),
+            CharacterAPI.init('Western Character #7'),
+        ],
+    };
+
     export function init(theme: GameTheme): Game {
         return {
             id: IdUtil.generateUUID(),
-            characters: new Set(),
-            murderers: new Set(),
+            murderers: [],
+            players: [],
             theme,
         };
     }
 
-    export function initCharactersForGame(
-        game: Game,
-        characterCount: number,
-    ): Game {
-        const initCharacters = (): Set<Character> => {
-            const characters = new Set<Character>();
-            for (let i = 0; i < characterCount; i++) {
-                characters.add(CharacterAPI.init());
-            }
-            return characters;
-        };
+    export function toString(game: Game): string {
+        const players = game.players.reduce(
+            (players: string, p: PlayerAPI.Player) => {
+                return players.concat(PlayerAPI.toString(p) + '\n');
+            },
+            '',
+        );
 
-        const gameCopy = Object.assign({}, game) as Game;
-        gameCopy.characters = initCharacters();
-        return gameCopy;
+        const murderers = game.murderers.reduce(
+            (murderers: string, m: PlayerAPI.Player) => {
+                return murderers.concat(PlayerAPI.toString(m) + '\n');
+            },
+            '',
+        );
+
+        return [
+            '=====================================================',
+            '[Game]\n',
+            'Game Theme\t' + game.theme,
+            'Players\t\t' + game.players.length,
+            'Murderers\t' + game.murderers.length,
+            '=====================================================',
+            '[Players]',
+            players,
+            '=====================================================',
+            '[Murderers]',
+            murderers,
+        ].join('\n');
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,35 @@
-import * as readline from 'readline';
 import { GameAPI } from './apis/game.api';
-import { GameTheme } from './types';
+import { PlayerAPI } from './apis/player.api';
 
 function main(): void {
-    // todo - @prompt - Select theme
-    const theme = GameTheme.Western;
-    let game = GameAPI.init(theme);
+    // todo - @prompt - Select theme for game
+    const theme = GameAPI.GameTheme.Western;
+    const game = GameAPI.init(theme);
 
-    // todo - @prompt - Choose number of characters
-    const characterCount = 4;
-    game = GameAPI.initCharactersForGame(game, characterCount);
+    // todo - @prompt - Choose number of players participating in the game
+    const playerCount = 4;
+    const players = PlayerAPI.initMany(playerCount);
+
+    // todo - @prompt - Players enter their own name
+    players[0].name = 'Player #1';
+    players[1].name = 'Player #2';
+    players[2].name = 'Player #3';
+    players[3].name = 'Player #4';
 
     // todo - @prompt? - Select difficulty (default is Moderate, [Easy, Moderate, Sherlock])
-    // todo - @prompt - Users select their characters
 
-    // todo - Generate relationships based off character selections
+    // todo - @prompt - Players select their characters (or allow random selection for player)
+    const availableCharacters = GameAPI.themeAvailableCharacters[theme];
+    players[0].character = availableCharacters[0];
+    players[1].character = availableCharacters[1];
+    players[2].character = availableCharacters[2];
+    players[3].character = availableCharacters[3];
+
+    game.players = players;
+
+    console.log(GameAPI.toString(game));
+
+    // todo - Generate relationships to murderer based off character selections
 }
 
 main();
